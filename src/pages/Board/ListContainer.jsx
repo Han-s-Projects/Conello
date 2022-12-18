5896b8446e7ff88f90d35a2ef7c5acb1bf7430d5
dikum98 <dikum98@gmail.com>
2022-12-18 10:20:20 +0900

    design: apply Drag & Drop style
    - 리스트 및 카드 드래그 시 기울기 효과
    - 드랍 될 리스트 배경 색 넣기
    - 비어있는 리스트에 카드 드랍 가능하게

--- src/pages/Board/ListContainer.jsx
+++ src/pages/Board/ListContainer.jsx
@@ -5,6 +5,22 @@ import { Droppable, Draggable } from "@hello-pangea/dnd";
 const ListContainer = ({ lists, setLists, cards, setCards }) => {
   console.log("ListContainer render");
 
+  const getListStyle = (style, snapshot) => {
+    if (!snapshot.isDropAnimating) {
+      return style;
+    }
+    const { moveTo, curve, duration } = snapshot.dropAnimation;
+    const translate = `translate(${moveTo.x}px, ${moveTo.y}px)`;
+    const rotate = "rotate(5deg)";
+    const scale = "scale(1.1)";
+
+    return {
+      ...style,
+      transform: `${translate} ${rotate} ${scale}`,
+      transition: `all ${curve} ${duration + 0.1}s`,
+    };
+  };
+
   return (
     <Droppable droppableId="lists" direction="horizontal" type="LIST">
       {(provided) => (
@@ -15,11 +31,13 @@ const ListContainer = ({ lists, setLists, cards, setCards }) => {
         >
           {lists.map((list, i) => (
             <Draggable key={list.id} draggableId={list.id} index={i}>
-              {(provided) => (
+              {(provided, snapshot) => (
                 <div
                   ref={provided.innerRef}
                   {...provided.draggableProps}
                   {...provided.dragHandleProps}
+                  isDragging={snapshot.isDragging && !snapshot.isDropAnimating}
+                  style={getListStyle(provided.draggableProps.style, snapshot)}
                 >
                   <List
                     list={list}
