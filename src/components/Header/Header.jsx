a11e47e69de171b2a77e0017321647fe89b0baba
dikum98 <dikum98@gmail.com>
2023-04-26 17:12:09 +0900

    fix: 로그아웃 상태에서 로고 클릭시 404 페이지로 이동하는 버그 수정
    - 홈페이지로 이동하도록 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,24 @@
-import styles from "./Header.module.css";
-import logo from "assets/conello_logo.svg";
-import { Link, useNavigate } from "react-router-dom";
+import styles from './Header.module.css';
+import logo from 'assets/conello_logo.svg';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    localStorage.removeItem("trello_token");
+    localStorage.removeItem('trello_token');
 
-    navigate("/");
+    navigate('/');
 
     window.location.reload();
   };
 
   return (
     <header className={styles.container}>
-      <Link to={"/boards"}>
+      <Link to={localStorage.getItem('trello_token') ? '/boards' : '/'}>
         <img className={styles.logo} src={logo} alt="logo" />
       </Link>
-      {localStorage.getItem("trello_token") ? (
+      {localStorage.getItem('trello_token') ? (
         <button className={styles.logout} onClick={handleLogout}>
           Log out
         </button>
