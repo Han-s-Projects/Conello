dc8085d89f3c32bb14e9da49acbc786839e58db1
dikum98 <dikum98@gmail.com>
2023-04-27 19:59:44 +0900

    remove: 불필요한 파일 삭제
    - 타입스크립트로 마이그레이션 하면서 불필요해진 jsconfig.json, 각종 js, jsx 파일들 삭제

--- src/components/Menu/Menu.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Menu.module.css";
-
-const Menu = ({ children }) => {
-  return <div className={styles.menu}>{children}</div>;
-};
-
-export default Menu;
--- src/pages/Board/Description/DescriptionPortal.jsx
+++
@@ -1,8 +0,0 @@
-import { createPortal } from "react-dom";
-
-const DescriptionPortal = ({ children }) => {
-  const el = document.getElementById("modal");
-  return createPortal(children, el);
-};
-
-export default DescriptionPortal;
