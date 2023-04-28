ab2bbd4397ed8c3e18f90cb10c0fd93117f587fd
dikum98 <dikum98@gmail.com>
2023-04-29 06:48:08 +0900

    refactor: Form 컴포넌트 타입 정의

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import CardForm from "./Form.module.css";
 import BoardForm from "./BoardForm.module.css";
 
-const Form = ({ placeholder, value, onChange, onSubmit }) => {
+interface Props {
+  placeholder: "Add a card" | string;
+  value: string;
+  onChange: (e: any) => void;
+  onSubmit: (e: any) => Promise<void>;
+}
+
+const Form = ({ placeholder, value, onChange, onSubmit }: Props) => {
   const styles = placeholder === "Add a card" ? CardForm : BoardForm;
   return (
     <form className={styles.form} onSubmit={onSubmit}>
