9e60717faa341540cf19f5c56e6fd0a583fb3b79
dikum98 <dikum98@gmail.com>
2023-04-29 16:07:37 +0900

    fix: 포탈 컴포넌트 타입 정의 및 예외 처리

--- src/pages/Board/Description/DescriptionPortal.tsx
+++ src/pages/Board/Description/DescriptionPortal.tsx
@@ -1,8 +1,21 @@
+import React from "react";
 import { createPortal } from "react-dom";
 
-const DescriptionPortal = ({ children }) => {
+interface Props {
+  children: React.ReactNode;
+}
+
+const DescriptionPortal = ({ children }: Props) => {
   const el = document.getElementById("modal");
-  return createPortal(children, el);
+
+  if (el) {
+    return createPortal(children, el);
+  } else {
+    console.error(
+      'DescriptionPortal 컴포넌트에 document.getElementById("modal")의 값이 null입니다.'
+    );
+    return null;
+  }
 };
 
 export default DescriptionPortal;
